ccd66f11e878a7d979dc61bd44f769be0c7b8acf
Nagy-Egri Máté Ferenc <csiga.biga@aol.com>
2019-03-20 16:53:30 +0100

    Vector class added

--- KutInf1.cpp
+++ KutInf1.cpp
@@ -1,4 +1,10 @@
+#include "Vector.hpp"
+
+#include <iostream>
 int main()
 {
-    return 0;
+    Vector<double> v{{1., 2., 3.}};
+    Vector<double> u{{10., 20., 30.}};
+    auto w = (v + std::move(u)) / 2.0;
+    std::cout << w[0] << "\n";
 }
